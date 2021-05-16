131e6c1662ba77b97207bb7cb01ed9ba575c34fe
RenatiusvanSander / R. Meier <remad@web.de>
2021-05-16 20:17:15 +0200

    main: Function Interface higher order function

--- src/main/java/remad/edu/methods/FunctionsInterface.java
+++ src/main/java/remad/edu/methods/FunctionsInterface.java
@@ -2,9 +2,20 @@ package remad.edu.methods;
 
 import java.util.function.Function;
 
-public class SeveralArguments {
+/**
+ * This demonstrates the power of {@link Function} / {@link FunctionalInterface}
+ */
+public class FunctionsInterface {
 
+  /**
+   * Some examples for what a function interface can be used.
+   *
+   * @param args the operating system parameters to start this method.
+   */
   public static void main(String[] args) {
+    Function<Integer, Integer> triple = x -> x * 3;
+    Function<Integer, Integer> square = x -> x * x;
+
     Function<Integer, Function<Integer, Integer>> add = x -> y -> x + y;
 
     int number = add.apply(3).apply(5);
@@ -13,8 +24,20 @@ public class SeveralArguments {
     BinaryOperator adds = x -> y -> x + y;
     int number_2 = adds.apply(3).apply(5);
     System.out.println(number_2);
+
+    BinaryOperator mul = x -> y -> x * y;
+    int number_3 = mul.apply(3).apply(5);
+    System.out.println(number_3);
+
+    //higher order function (HOF)
+    Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>> compose = x -> y -> z -> x
+        .apply(y.apply(z));
+    Function<Integer, Integer> f = compose.apply(square).apply(triple);
+    System.out.println(f.apply(2));
   }
 
-  public interface BinaryOperator extends Function<Integer, Function<Integer, Integer>> {}
+  public interface BinaryOperator extends Function<Integer, Function<Integer, Integer>> {
+    // empty
+  }
 
 }
